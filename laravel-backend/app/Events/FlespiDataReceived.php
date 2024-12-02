f7b0f58ff276df6b26f74a89f77c1403d6b517e6
Khen Harold Paler <paler.khen.ustp@gmail.com>
2024-12-02 11:03:34 +0800

    broadcasting the flespi data using laravel echo and pusher

---
+++ laravel-backend/app/Events/FlespiDataReceived.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class FlespiDataReceived implements ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('flespi-data'); // Public or private channel
+    }
+}
+
+
--- laravel-backend/app/Http/Controllers/FlespiController.php
+++ laravel-backend/app/Http/Controllers/FlespiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Events\FlespiDataReceived;
 
 class FlespiController extends Controller
 {
@@ -11,14 +12,14 @@ class FlespiController extends Controller
     {
         $data = $request->all();
 
-        // Check if tracker is moving
         if (isset($data['PositionSpeed']) && $data['PositionSpeed'] > 0) {
-            // Process data for moving tracker
             Log::info('Tracker is moving:', $data);
         } else {
-            // Skip processing for stationary tracker
             Log::info('Tracker is stationary:', $data);
         }
+
+        // Broadcast the data to the frontend
+        broadcast(new FlespiDataReceived($data));
 
         return response()->json(['status' => 'success']);
     }
