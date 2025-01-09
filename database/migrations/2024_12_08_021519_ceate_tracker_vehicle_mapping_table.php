3efc53b50b4a266b08edb2e41fc9ab17c7a4029c
khenpro123 <paler.khen.ustp@gmail.com>
2025-01-09 11:21:21 +0800

    update

--- database/migrations/2024_12_08_021519_ceate_tracker_vehicle_mapping_table.php
+++ database/migrations/2024_12_08_021519_ceate_tracker_vehicle_mapping_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('tracker_vehicle_mapping', function (Blueprint $table) {
             $table->id(); // Primary key
             $table->string('device_name');
-            $table->string('tracker_ident', 50)->unique();
+            $table->string('tracker_ident', 50)->unique()->nullable();
             $table->string('vehicle_id', 10); 
             $table->enum('status', ['active', 'inactive']);
             $table->unsignedBigInteger('created_by'); 
